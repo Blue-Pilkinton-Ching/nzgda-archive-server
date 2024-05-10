cca70860dacaa0afb0485e9a113adfebfa0b5f98
Blue Pilkinton-Ching <bluepilkintonching@gmail.com>
2024-05-10 14:27:01 +1200

    hopefully fixed CORS thing

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,19 @@ app.use(
     exposedHeaders: ['privilege'],
   })
 )
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  )
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
+  }
+  next()
+})
 app.use(express.json())
 app.use(express.text())
 app.use(express.urlencoded({ extended: true }))
