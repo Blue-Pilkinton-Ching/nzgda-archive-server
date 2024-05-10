618e8121a3c0014be8e33a7ae197b1699edeef34
Blue Pilkinton-Ching <bluepilkintonching@gmail.com>
2024-05-10 14:28:33 +1200

    Another change

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,23 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
+import https from 'https'
+import fs from 'fs'
 
 import multer from 'multer'
 
 const app = express()
 const PORT = process.env.PORT || 3002
+
+// SSL certs files
+const privateKey = fs.readFileSync(
+  '/home/ubuntu/websites/ssl/server.key',
+  'utf8'
+)
+const certificate = fs.readFileSync(
+  '/home/ubuntu/websites/ssl/server.crt',
+  'utf8'
+)
+const credentials = { key: privateKey, cert: certificate }
 
 app.use(
   cors({
@@ -24,6 +37,7 @@ app.use((req, res, next) => {
   }
   next()
 })
+
 app.use(express.json())
 app.use(express.text())
 app.use(express.urlencoded({ extended: true }))
@@ -40,6 +54,12 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Heihei website api backend!')
 })
 
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
+//   console.log(`Server started on port ${PORT}`)
+// })
+
+//start ssl server
+const httpsServer = https.createServer(credentials, app)
+httpsServer.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
 })
