779482e3d1d75f03df3b896ff4669bc08832a866
Blue Pilkinton-Ching <bluepilkintonching@gmail.com>
2024-06-27 18:56:15 +1200

    approval of games is now working

--- src/routes/game.ts
+++ src/routes/game.ts
@@ -44,3 +44,32 @@ game.post('/', async (req, res) => {
     res.status(401).send('Unauthorized')
   }
 })
+
+game.patch('/:gameID', async (req, res) => {
+  const privilege = req.headers['privilege'] as UserPrivilege
+  const gameID = req.params.gameID
+
+  if (privilege === 'admin') {
+    const data = req.body
+
+    if (!gameID) {
+      res.status(400).send('Missing game ID')
+      return
+    }
+
+    connection.query(
+      'UPDATE games SET ? WHERE id = ?',
+      [data, gameID],
+      (err) => {
+        if (err) {
+          console.error(err)
+          res.status(500).send('Internal Server error')
+          return
+        }
+        res.send('Game updated')
+      }
+    )
+  } else {
+    res.status(401).send('Unauthorized')
+  }
+})
