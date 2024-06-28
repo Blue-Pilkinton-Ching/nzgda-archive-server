8261de96ae141bdd789d934c3252452ddb71901e
Blue Pilkinton-Ching <bluepilkintonching@gmail.com>
2024-06-28 16:43:19 +1200

    got studio editting working

--- src/routes/studios.ts
+++ src/routes/studios.ts
@@ -32,3 +32,45 @@ studios.post('/', (req, res) => {
     res.status(401).send('Unauthorized')
   }
 })
+
+studios.patch('/', (req, res) => {
+  const privilege = req.headers['privilege'] as UserPrivilege
+  const studio = req.headers['studio'] as string
+
+  if (privilege === 'admin' && Number(studio) === 0) {
+    connection.query(
+      'UPDATE studios SET ? WHERE id = ?',
+      [req.body, req.body.id],
+      (err) => {
+        if (err) {
+          console.error(err)
+          return res.status(500).send('Internal server error')
+        }
+        res.status(200).send('Success')
+      }
+    )
+  } else {
+    res.status(401).send('Unauthorized')
+  }
+})
+
+studios.delete('/', (req, res) => {
+  const privilege = req.headers['privilege'] as UserPrivilege
+  const studio = req.headers['studio'] as string
+
+  if (privilege === 'admin' && Number(studio) === 0) {
+    connection.query(
+      'DELETE FROM studios WHERE id = ?',
+      [req.body.id],
+      (err) => {
+        if (err) {
+          console.error(err)
+          return res.status(500).send('Internal server error')
+        }
+        res.status(200).send('Success')
+      }
+    )
+  } else {
+    res.status(401).send('Unauthorized')
+  }
+})
