750a6044ddc162f2ec5accb5311d4516b5cf3842
Blue <whatsup@bluepc.me>
2025-05-22 17:23:49 +1200

    updated migrations

--- migrations.sql
+++ migrations.sql
@@ -1,22 +1,28 @@
 CREATE TABLE games (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    date_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    description TEXT NOT NULL,
     name TEXT NOT NULL,
-    thumbnail VARCHAR(255) NULL ,
-    banner VARCHAR(255) NULL,
-    url VARCHAR(511) NULL,
+    description TEXT NOT NULL,
+    thumbnail VARCHAR(255) NOT NULL,
+    banner VARCHAR(255), -- nullable
     approved BOOLEAN NOT NULL,
     featured BOOLEAN NOT NULL,
     educational BOOLEAN NOT NULL,
-    tags VARCHAR(255),
-    width INT NULL,
-    height INT NULL,
+    hidden BOOLEAN NOT NULL,
     isApp BOOLEAN NOT NULL,
     studio_id INT NOT NULL,
+    url VARCHAR(511), -- nullable
+    tags VARCHAR(255),
+    width INT,
+    height INT,
     sort INT,
-    hidden BOOLEAN NOT NULL
+    yearOfRelease INT NOT NULL,
+    iosLink VARCHAR(511),
+    androidLink VARCHAR(511),
+    steamLink VARCHAR(511),
+    websiteLink VARCHAR(511),
+    otherLinks TEXT,
+    date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    date_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE admins (
@@ -35,9 +41,19 @@ CREATE TABLE requests (
 CREATE TABLE studios (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL
+  ALTER TABLE studio
+  steam VARCHAR(255),
+  ios VARCHAR(255),
+  android VARCHAR(255),
+  cityOrRegion VARCHAR(255),
+  yearFounded INT,
+  otherLinks TEXT,
+  website VARCHAR(255),
+  description TEXT,
+  date_created DATETIME DEFAULT CURRENT_TIMESTAMP,
+  date_updated DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  nzgdaMember BOOLEAN DEFAULT FALSE;
 );
-
-DELIMITER $$
 
 CREATE TRIGGER set_sort_value
 AFTER INSERT ON games
@@ -45,5 +61,3 @@ FOR EACH ROW
 BEGIN
     UPDATE games SET sort = NEW.id * 100 WHERE id = NEW.id;
 END$$
-
-DELIMITER ;
